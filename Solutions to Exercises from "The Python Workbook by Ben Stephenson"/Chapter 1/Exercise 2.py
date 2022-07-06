6f3e469dff0809deae44876e1b21b6f0862d370c
Jolly Chukwuemeka <Jolliano@ymail.com>
2022-07-06 14:10:31 +0100

    Update Exercise 2.py

--- Solutions to Exercises from "The Python Workbook by Ben Stephenson"/Chapter 1/Exercise 2.py
+++ Solutions to Exercises from "The Python Workbook by Ben Stephenson"/Chapter 1/Exercise 2.py
@@ -1,6 +1,6 @@
 # Prints a welcome message using the name you input
 
-fname = input{"Please enter your first name: "}
+fname = input("Please enter your first name: ")
 lname = input("Please enter your second name: ")
 
-print("You are welcome, " + fname + lname)
+print("You are welcome, " + fname + " " + lname)
