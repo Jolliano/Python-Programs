93e2737e0c77ca87bc58ebf15c56dd82250bf7c0
Jolly Chukwuemeka <Jolliano@ymail.com>
2022-07-06 14:07:50 +0100

    Create Exercise 2.py
    Write a program that asks the user to enter his or her name. The program should
    respond with a message that says hello to the user, using his or her name.

---
+++ Solutions to Exercises from "The Python Workbook by Ben Stephenson"/Chapter 1/Exercise 2.py
@@ -0,0 +1,6 @@
+# Prints a welcome message using the name you input
+
+fname = input{"Please enter your first name: "}
+lname = input("Please enter your second name: ")
+
+print("You are welcome, " + fname + lname)
